476d4a1e5db7224d5a7cf4f390996c02b4167ff4
OlaoluwaM <37044906+OlaoluwaM@users.noreply.github.com>
2023-07-21 09:12:44 -0500

    feat: program1 works

--- src/Basic.hs
+++ src/Basic.hs
@@ -1,12 +1,13 @@
 module Basic (addBook, removeBook, lookupBookByAuthor, lookupBookByISBN, lookupBookByTitle, displayBookAvailability, borrowBook, returnBook) where
 
-import Control.Monad.Except (ExceptT, MonadError (throwError), liftEither)
+import Control.Monad.Except (ExceptT, MonadError (throwError), liftEither, runExceptT)
 import Control.Monad.State
 import Control.Monad.Writer
 import Data.List qualified as List
 import Data.Map (Map)
 import Data.Map qualified as Map
-import Data.Text (Text)
+import Data.Text (Text, intercalate)
+import Data.Text.IO qualified as TIO
 import Data.Time
 import Helpers
 import PyF (fmt)
@@ -48,7 +49,7 @@ data LibrarySysError = NoCopies OptionalErrorContext | BookNotFound OptionalErro
 
 type Library = Map Text LibraryEntry
 
-type BasicStack a = WriterT Text (ExceptT LibrarySysError (State Library)) a
+type BasicStack a = WriterT [Text] (ExceptT LibrarySysError (State Library)) a
 
 -- ------------------------------ Athenaeum Basic API ----------------------------- --
 addBook :: Book -> BasicStack LibraryEntry
@@ -57,14 +58,13 @@ addBook newBook@(Book newBookTitle _ _) = do
   let bookAvailabilityStatus = lookupBookByTitle currentLibrary newBookTitle
 
   let (updatedLibrary, msg, bookLibEntry) = case bookAvailabilityStatus of
-        Right bookLibEntry' -> (incrementBookCopiesCount currentLibrary, newCopyMsg, bookLibEntry')
+        Right bookLibEntry' -> (incrementBookCopiesCount currentLibrary newBookTitle, newCopyMsg, bookLibEntry')
         Left _ -> let (updatedLib, newLibEntry) = addNewBookToLibrary currentLibrary in (updatedLib, newBookMsg, newLibEntry)
 
   put updatedLibrary
 
-  writer (bookLibEntry, msg)
+  writer (bookLibEntry, [msg])
   where
-    incrementBookCopiesCount = Map.adjust (modifyBookCopyCount (+ 1)) newBookTitle
     addNewBookToLibrary currentLib = let newLibEntry = LibraryEntry {bookInfo = newBook, copies = 1, borrowedBy = []} in (Map.insert newBookTitle newLibEntry currentLib, newLibEntry)
 
     newBookMsg = [fmt|Added the book '{newBookTitle}' to the library!|]
@@ -86,40 +86,41 @@ removeBook bookTitle = do
 
   let (updatedLibrary, msg, maybeBookLibEntry) =
         if copiesCount > 0
-          then (decrementBookCopiesCount currentLibrary, copyRemovedMsg, Just bookLibEntry)
+          then (decrementBookCopiesCount currentLibrary bookTitle, copyRemovedMsg, Just bookLibEntry)
           else (deleteBookFromLibrary currentLibrary, bookDeletedMsg, Nothing)
 
   put updatedLibrary
-  writer (maybeBookLibEntry, msg)
+  writer (maybeBookLibEntry, [msg])
   where
-    decrementBookCopiesCount = Map.adjust (modifyBookCopyCount (subtract 1)) bookTitle
     deleteBookFromLibrary = Map.delete bookTitle
 
     copyRemovedMsg = [fmt|Removed a copy of the book '{bookTitle}'|]
     bookDeletedMsg = [fmt|Deleted the book '{bookTitle}' from library|]
 
-displayBookAvailability :: Text -> BasicStack Text
+displayBookAvailability :: Text -> BasicStack ()
 displayBookAvailability bookTitle = do
   lib <- get
   let bookAvailabilityStatus = lookupBookByTitleIncludingBooksWithoutCopies lib bookTitle
+  let copyTense copiesCount = if copiesCount == 1 then "copy" else "copies"
 
   let bookAvailabilityMsg = case bookAvailabilityStatus of
-        Right (LibraryEntry _ copiesCount _) -> [fmt|We currently have, {copiesCount}, of the book, '{bookTitle}'|]
+        Right (LibraryEntry _ copiesCount _) -> [fmt|We currently have {copiesCount} {(copyTense copiesCount) :: Text} of the book '{bookTitle}'|]
         Left (NoCopies _) -> [fmt|Sorry, it looks like all copies of the book, '{bookTitle}' have either been borrowed or deleted|]
-        Left (BookNotFound _) -> [fmt|Sorry, it looks like the book, '{bookTitle}' does not exist, perhaps it was removed|]
+        Left (BookNotFound _) -> [fmt|Sorry, it looks like the book, '{bookTitle}' does not exist, perhaps it was removed or not yet added|]
         Left (CustomError (ErrorContext errContext)) -> [fmt|The following error occurred when attempting to display the availability of the book '{bookTitle}': {errContext}|]
         Left _ -> "An error Occurred"
 
-  return bookAvailabilityMsg
+  tell [bookAvailabilityMsg]
 
 borrowBook :: (Day, Integer) -> Text -> Patron -> BasicStack Patron
 borrowBook (currentDay, randomOffsetForDeadline) bookTitle patron = do
-  libraryEntry <- removeBook bookTitle >>= lift . liftEither . maybeToEither (bookNotBorrowableErr bookTitle)
+  currentLib <- get
+  libraryEntry <- lift $ liftEither $ (`lookupBookByTitle` bookTitle) $ decrementBookCopiesCount currentLib bookTitle
 
   modify (`trackTheBorrower` libraryEntry)
   let updatedPatron = trackTheBorrowedBook patron libraryEntry
 
-  writer (updatedPatron, outputMsg)
+  writer (updatedPatron, [outputMsg])
   where
     trackTheBorrower lib bookLibEntry@(LibraryEntry (Book bookTitle' _ _) _ patrons) =
       Map.adjust (const $ bookLibEntry {borrowedBy = patron : patrons}) bookTitle' lib
@@ -131,7 +132,6 @@ borrowBook (currentDay, randomOffsetForDeadline) bookTitle patron = do
 
     returnDeadline' = addDays randomOffsetForDeadline currentDay
     outputMsg = [fmt|Thanks! You just borrowed the book '{bookTitle}'. Your return deadline is {tshow returnDeadline'}|]
-    bookNotBorrowableErr bookTitle' = NoCopies $ Just $ ErrorContext {context = [fmt|Sorry! It looks like we don't have any copies of the book '{bookTitle' :: Text}' to give out|]}
 
 returnBook :: Day -> Patron -> Book -> BasicStack Patron
 returnBook currentDay patron@(Patron patronName _ borrowedBooks) borrowedBook@(Book bookTitle _ _) = do
@@ -146,7 +146,7 @@ returnBook currentDay patron@(Patron patronName _ borrowedBooks) borrowedBook@(B
 
   when (deadlineOffset < 0) $ throwError $ PastDue $ Just $ ErrorContext [fmt|You are returning this book {abs deadlineOffset} days past due!|]
 
-  writer (updatedPatron, [fmt|Thanks {patronName}! You just returned the book '{bookTitle}'|])
+  writer (updatedPatron, [[fmt|Thanks {patronName}! You just returned the book '{bookTitle}'|]])
   where
     unTrackTheBorrower lib bookLibEntry@(LibraryEntry (Book bookTitle' _ _) _ patrons) =
       Map.adjust (const $ bookLibEntry {borrowedBy = filter (/= patron) patrons}) bookTitle' lib
@@ -158,6 +158,12 @@ returnBook currentDay patron@(Patron patronName _ borrowedBooks) borrowedBook@(B
     wrongBorrowerMsg = "Oops, looks like you weren't the one who borrowed this book" :: Text
 
 -- -------------------------- Athenaeum Basic API Helpers ------------------------- --
+incrementBookCopiesCount :: Library -> Text -> Library
+incrementBookCopiesCount lib bookTitle = Map.adjust (modifyBookCopyCount (+ 1)) bookTitle lib
+
+decrementBookCopiesCount :: Library -> Text -> Library
+decrementBookCopiesCount lib bookTitle = Map.adjust (modifyBookCopyCount (subtract 1)) bookTitle lib
+
 modifyBookCopyCount :: (Integer -> Integer) -> LibraryEntry -> LibraryEntry
 modifyBookCopyCount copyModFn libBookEntry@(LibraryEntry _ currentCopyCount _) = libBookEntry {copies = updatedCopyCount}
   where
@@ -181,21 +187,51 @@ lookupBookByBookProperty lib bookFieldLabel errorMsg bookPropertyToSearchBy =
     bookPropertyLookup (_, LibraryEntry bookInfo' _ _) = bookPropertyToSearchBy == bookFieldLabel bookInfo'
     addErrorMsg = maybeToEither . BookNotFound . Just . ErrorContext
 
+getLibSysErrorContext :: LibrarySysError -> Text
+getLibSysErrorContext = \case
+  NoCopies mErrContext -> foo mErrContext ""
+  PastDue mErrContext -> foo mErrContext ""
+  BookNotFound mErrContext -> foo mErrContext ""
+  CustomError (ErrorContext context') -> context'
+  where
+    foo maybeErrContext defaultMsg = case maybeErrContext of
+      Just (ErrorContext context') -> context'
+      Nothing -> defaultMsg
+
+-- ---------------------------------- Main ---------------------------------- --
 getDay :: IO Day
 getDay = utctDay <$> getCurrentTime
 
--- ---------------------------------- Main ---------------------------------- --
 firstBookInLibrary :: Book
 firstBookInLibrary = Book {title = "Production Haskell", author = "Matt Parsons", isbn = "ISBN-13: 978-3-16-148410-0"}
 
 library :: Library
 library = Map.empty
 
--- main :: IO ()
--- main = do
---   currentDay <- getDay
+program1 :: BasicStack ()
+program1 = do
+  let firstBookTitle = title firstBookInLibrary
+  tell [[fmt|Stocking the book {firstBookTitle}...|]]
+  _ <- displayBookAvailability firstBookTitle
+  _ <- addBook firstBookInLibrary
+  _ <- displayBookAvailability firstBookTitle
+  _ <- addBook firstBookInLibrary
+  _ <- addBook firstBookInLibrary
+  _ <- displayBookAvailability firstBookTitle
+  _ <- addBook firstBookInLibrary
+  _ <- displayBookAvailability firstBookTitle
+  _ <- removeBook firstBookTitle
+  _ <- removeBook firstBookTitle
+  _ <- displayBookAvailability firstBookTitle
+  tell ["Book Stocking complete"]
 
---   undefined
+main :: IO ()
+main = do
+  print "Running Program 1...."
+  let program = evalState (runExceptT (runWriterT program1)) library
+  let programOutput = either getLibSysErrorContext (intercalate "\n" . snd) program
+  TIO.putStrLn programOutput
+  print "Program 1 complete"
 
 -- -- Not sure why `stack runghc Solution.hs` isn't working, but using `stack ghci` and running main interactively does the trick
 -- main :: IO ()
